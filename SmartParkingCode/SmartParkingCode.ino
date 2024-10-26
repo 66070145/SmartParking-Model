4862ee246723c1cea05f69aeaab116fe534fdef0
Patlapa <141336257+66070149@users.noreply.github.com>
2024-10-26 12:45:28 +0700

    Update SmartParkingCode.ino

--- SmartParkingCode/SmartParkingCode.ino
+++ SmartParkingCode/SmartParkingCode.ino
@@ -1,32 +1,4 @@
-/*************************************************************
-  Blynk is a platform with iOS and Android apps to control
-  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build mobile and web interfaces for any
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: https://www.blynk.io
-    Sketch generator:           https://examples.blynk.cc
-    Blynk community:            https://community.blynk.cc
-    Follow us:                  https://www.fb.com/blynkapp
-                                https://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-  This example shows how to use Arduino WiFi shield
-  to connect your project to Blynk.
-
-  Please update your shield firmware:
-    https://www.arduino.cc/en/Hacking/WiFiShieldFirmwareUpgrading
-
-  Feel free to apply it to any other example. It's simple!
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
-/* Fill in information from Blynk Device Info here */
 #define BLYNK_TEMPLATE_ID "TMPL6QRjPH69I"
 #define BLYNK_TEMPLATE_NAME "smartparking"
 #define BLYNK_AUTH_TOKEN    "po6V3XG9qb7bnsUw9g21VbVpY7JBvgYo"
@@ -37,8 +9,6 @@
 #include <BlynkSimpleWifi.h>
 #include <Servo.h>
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "NT wifi 4106-2.4G";
 char pass[] = "0957959968";
 
@@ -71,13 +41,11 @@ int park_limit = 3;
 
 void setup()
 {
-  // Debug console
-  Serial.begin(9600);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 
   Blynk.virtualWrite(V2, park_limit);
-  pinMode(ledPin, OUTPUT);          // ตั้งค่าพิน LED เป็น OUTPUT
+  pinMode(ledPin, OUTPUT);
   gate_in.attach(10);
   gate_out.attach(11);
   pinMode(in_sensor, INPUT);
@@ -101,7 +69,7 @@ void loop()
         delay(100);
         park_counter -= 1;
         displayDigit(park_counter);
-        Blynk.virtualWrite(V0, park_counter);  // ส่งค่า park_counter ไปยัง Blynk
+        Blynk.virtualWrite(V0, park_counter);
         delay(2000);
         gate_in.write(0);
         delay(100);
@@ -112,7 +80,7 @@ void loop()
         delay(1000);
         park_counter += 1;
         displayDigit(park_counter);
-        Blynk.virtualWrite(V0, park_counter);  // ส่งค่า park_counter ไปยัง Blynk
+        Blynk.virtualWrite(V0, park_counter);
         delay(2000);
         gate_out.write(0);
         delay(100);
@@ -121,39 +89,28 @@ void loop()
 }
 
 BLYNK_WRITE(V1) {
-    int ledState = param.asInt(); // รับค่าจาก Blynk App (1 = เปิด, 0 = ปิด)
-    digitalWrite(ledPin, ledState); // ควบคุมสถานะ LED ตามค่าที่ได้รับ
+    int ledState = param.asInt();
+    digitalWrite(ledPin, ledState);
 }
 
 BLYNK_WRITE(V2) {
-    int new_park_limit = param.asInt();  // รับค่าจากแถบเลื่อน Blynk (V2) สำหรับกำหนด park_limit ใหม่
-  Serial.print("New park limit set to: ");
-  Serial.println(new_park_limit);
+    int new_park_limit = param.asInt();
 
-  // ตรวจสอบจำนวนที่จอดรถ (park_limit) ที่เปลี่ยนแปลง
   if (new_park_limit > park_limit) {
-    // หากเพิ่มจำนวนที่จอดใหม่ ให้ปรับเพิ่มค่า park_counter ให้สอดคล้อง
     park_counter += (new_park_limit - park_limit);
-    Serial.print("Park counter increased to: ");
-    Serial.println(park_counter);
+    displayDigit(park_counter);
   } else if (new_park_limit < park_limit) {
-    // หากลดจำนวนที่จอด และปัจจุบันมีรถจอดอยู่เกิน limit ใหม่ ให้ลดค่า park_counter ให้สอดคล้อง
     if (park_counter > new_park_limit) {
-      park_counter = new_park_limit;
-      Serial.print("Park counter decreased to: ");
-      Serial.println(park_counter);
+      park_counter =  max(0, park_counter - (park_limit - new_park_limit));
+      displayDigit(park_counter);
     }
   }
 
-  // ตั้งค่า park_limit ใหม่
   park_limit = new_park_limit;
-
-  // ส่งค่า park_counter ไปยัง Blynk App เพื่ออัปเดตหน้าจอ
   Blynk.virtualWrite(V0, park_counter);
 }
 
 void displayDigit(int digit) {
-  // Set each segment according to the num_array for the given digit
   digitalWrite(segment_a, num_array[digit][0]);
   digitalWrite(segment_b, num_array[digit][1]);
   digitalWrite(segment_c, num_array[digit][2]);
